468011e2ca04251a75b1abc69f5132a24fa66725
orimomo <mmko5110@gmail.com>
2020-08-02 18:50:18 +0900

    projectを追加

---
+++ VisionFrameworkTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VisionFrameworkTest
+//
+//  Created by orimomo on 2020/08/02.
+//  Copyright © 2020 orimomo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
